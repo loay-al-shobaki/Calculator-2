6b8521a71bf98f60cc89e1c21c8c877bab1daf09
Loay Al Shobaki <107764558+loay-al-shobaki@users.noreply.github.com>
2023-08-14 23:10:20 +0300

    create project

---
+++ app/src/main/java/com/example/calculator2/ui/theme/Color.kt
@@ -0,0 +1,30 @@
+package com.example.calculator2.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val MediumGray = Color(0xFF2E2E2E)
+val LightGray = Color(0xFF818181)
+val Orange = Color(0xFFFF9800)
+//---------------------------
+
+
+val OuterSpace300 = Color(0xFF303841)
+val OuterSpace200 = Color(0xFF2E373F)
+
+val ResultShadowColorTop = Color(0x0DFFFFFF)
+val ResultShadowColorBottom = Color(0x4D000000)
+val ButtonShadowColorTop = Color(0x40FFFFFF)
+val ButtonShadowColorBottom = Color(0X2E000000)
+
+val ButtonPink = Color(0XFFFB78A6)
+val ButtonBlue = Color(0XFF78CCFB)
+val ButtonYellow = Color(0XFFE9D30E)

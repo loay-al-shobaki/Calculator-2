bebc60031193dfc2d8778bb6a969f4209476cdda
Loay Al Shobaki <107764558+loay-al-shobaki@users.noreply.github.com>
2023-08-15 14:43:36 +0300

    finsh calculator

--- app/src/main/java/com/example/calculator2/ui/theme/Type.kt
+++ app/src/main/java/com/example/calculator2/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.example.calculator2.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -11,10 +12,15 @@ val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 25.sp,
+        color = Color.White,
     ),
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 41.sp,
+        color = Color.White
+    )
 
 
     /* Other default text styles to override
